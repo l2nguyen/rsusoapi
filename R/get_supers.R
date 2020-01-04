2facd6001fa91e8b87da7b5ecc64f262a45c58cf
Lena Nguyen <lena.nguyen4@gmail.com>
2020-01-04 18:21:26 +0000

    Initial commit of function to get list of supervisors

---
+++ R/get_supers.R
@@ -0,0 +1,82 @@
+get_supers <- function(server, user, password) {
+  # check that server, user, password are non-missing and strings
+  for (x in c("server", "user", "password")) {
+    if (!is.character(get(x))) {
+      stop(x, "has to be a string.")
+    }
+    if (nchar(get(x)) == 0) {
+      stop(paste("The following parameter is not specified in the program:", x))
+    }
+  }
+
+  server <- tolower(trimws(server))
+
+  # check server exists
+  server_url <- paste0("https://", server, ".mysurvey.solutions")
+
+  # Check server exists
+  tryCatch(httr::http_error(server_url),
+           error=function(err) {
+             err$message <- paste(server, "is not a valid server.")
+             stop(err)
+           })
+
+  # build base URL for API
+  api_url <- paste0(server_url, "/api/v1")
+
+  # build query
+  endpoint <- paste0(api_url, "/supervisors")
+
+  # initial call to test API works
+  data <- httr::GET(endpoint, httr::authenticate(user, password),
+                    query= list(limit=40))
+
+  # if API call works, get supervisor information
+  if (httr::status_code(data) == 200) {
+    # save the list of imported templates from the API as a data frame
+    sups <- jsonlite::fromJSON(content(data, as = "text"), flatten = TRUE)
+    super_df <- sups$Users
+    df_list <- list(super_df)
+    # get total count for iteration
+    total_count <- sups$TotalCount
+  } else if (httr::status_code(data) == 401) {# login error
+    stop("Incorrect username or password. Check login credentials.")
+  } else {# any other error
+    stop("Encountered issue with status code ", status_code(data))
+  }
+
+  # if less than 40, return only data frame in list
+  if (total_count<=40){
+    sups_df <- df_list[[1]]
+
+    sups_df <- sups_df %>%
+      dplyr::select(UserName, UserId, IsLocked, CreationDate, DeviceId) %>%
+      dplyr::rename(SuperId = UserId, SuperName = UserName)
+  } else{
+    # use limit to figure out number of calls to make
+    limit <- sups$Limit
+    n_calls <- ceiling(total_count/limit)
+
+    for (i in 2:n_calls){
+      loop_resp <- httr::GET(endpoint, httr::authenticate(user, password),
+                             query= list(limit=40, offset=i))
+
+      if (status_code(loop_resp) == 200) {
+        # process response
+        flat_loop <- jsonlite::fromJSON(content(loop_resp, as = "text"),
+                                        flatten = TRUE)
+        loop_df <- flat_loop$Users
+        # append to existing list of df
+        df_list[[i]] <- loop_df
+      } else {# any other error
+        stop("Encountered issue with status code ", status_code(loop_resp))
+      }
+    }
+    # bind all dataframes with supervisor info together
+    sups_df <- dplyr::bind_rows(df_list) %>%
+      dplyr::select(UserName, UserId, IsLocked, CreationDate, DeviceId) %>%
+      dplyr::rename(SuperId = UserId, SuperName = UserName)
+  }
+  # return data frame with supervisors
+  return(sups_df)
+}

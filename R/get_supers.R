4e232426594cbc03dafaba0c767c1fcc99151c47
Lena Nguyen <lena.nguyen4@gmail.com>
2020-01-04 18:30:48 +0000

    Added documentation

--- R/get_supers.R
+++ R/get_supers.R
@@ -1,3 +1,33 @@
+#' Get data frame of supervisors on the server
+#'
+#' \code{get_supers} Returns a data frame containing information about supervisors
+#' account. Due to the current limitations of the API, only supervisors that are
+#' \strong{not archived/locked} appear in the output data frame.
+#' The data frame will have the following columns:
+#' \itemize{
+#' \item SuperName: User name of supervisors
+#' \item SuperId: Unique ID of supervisors
+#' \item IsLocked: If supervisor is currently locked/archived
+#' \item CreationDate: Date the supervisor account was created
+#' \item DeviceId: Unique Id of the tablet that the user was using on the last
+#' synchronisation to server. If the user has never synced any data with the server
+#' using a tablet, this variable will be null.
+#' }
+#'
+#' @param server Prefix for the survey server. It is whatever comes before
+#' mysurvey.solutions: [prefix].mysurvey.solutions.
+#' @param user Username for the API user on the server.
+#' @param password Password for the API user on the server.
+#'
+#' @importFrom rlang .data
+#' @export
+#'
+#' @return A data frame  data frame containining information about supervisor accounts.
+#' @examples
+#' \dontrun{
+#' get_supers(server = "lfs2018", user = "APIuser2018", password = "SafePassword123")
+#' }
+
 get_supers <- function(server, user, password) {
   # check that server, user, password are non-missing and strings
   for (x in c("server", "user", "password")) {
@@ -34,7 +64,7 @@ get_supers <- function(server, user, password) {
   # if API call works, get supervisor information
   if (httr::status_code(data) == 200) {
     # save the list of imported templates from the API as a data frame
-    sups <- jsonlite::fromJSON(content(data, as = "text"), flatten = TRUE)
+    sups <- jsonlite::fromJSON(httr::content(data, as = "text"), flatten = TRUE)
     super_df <- sups$Users
     df_list <- list(super_df)
     # get total count for iteration
@@ -42,16 +72,12 @@ get_supers <- function(server, user, password) {
   } else if (httr::status_code(data) == 401) {# login error
     stop("Incorrect username or password. Check login credentials.")
   } else {# any other error
-    stop("Encountered issue with status code ", status_code(data))
+    stop("Encountered issue with status code ", httr::status_code(data))
   }
 
   # if less than 40, return only data frame in list
   if (total_count<=40){
     sups_df <- df_list[[1]]
-
-    sups_df <- sups_df %>%
-      dplyr::select(UserName, UserId, IsLocked, CreationDate, DeviceId) %>%
-      dplyr::rename(SuperId = UserId, SuperName = UserName)
   } else{
     # use limit to figure out number of calls to make
     limit <- sups$Limit
@@ -61,22 +87,26 @@ get_supers <- function(server, user, password) {
       loop_resp <- httr::GET(endpoint, httr::authenticate(user, password),
                              query= list(limit=40, offset=i))
 
-      if (status_code(loop_resp) == 200) {
+      if (httr::status_code(loop_resp) == 200) {
         # process response
-        flat_loop <- jsonlite::fromJSON(content(loop_resp, as = "text"),
+        flat_loop <- jsonlite::fromJSON(httr::content(loop_resp, as = "text"),
                                         flatten = TRUE)
         loop_df <- flat_loop$Users
         # append to existing list of df
         df_list[[i]] <- loop_df
       } else {# any other error
-        stop("Encountered issue with status code ", status_code(loop_resp))
+        stop("Encountered issue with status code ", httr::status_code(loop_resp))
       }
     }
     # bind all dataframes with supervisor info together
-    sups_df <- dplyr::bind_rows(df_list) %>%
-      dplyr::select(UserName, UserId, IsLocked, CreationDate, DeviceId) %>%
-      dplyr::rename(SuperId = UserId, SuperName = UserName)
+    sups_df <- dplyr::bind_rows(df_list)
   }
+
+  output_df <- sups_df %>%
+    dplyr::select(.data$UserName, .data$UserId, .data$IsLocked,
+                  .data$CreationDate, .data$DeviceId) %>%
+    dplyr::rename(SuperId = .data$UserId, SuperName = .data$UserName)
+
   # return data frame with supervisors
-  return(sups_df)
+  return(output_df)
 }
